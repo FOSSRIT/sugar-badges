d5e340d465b393aa474c3c54e6a33f7f200503f3
David Wilson <daw4475@g.rit.edu>
2013-10-21 14:11:42 -0400

    0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='sugar-badges',
-    version='0.1.0',
+    version='0.1.1',
     description='Award badges in Sugar',
     author="David Wilson",
     author_email="daw4475@rit.edu",
